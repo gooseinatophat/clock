6a09cd92460ba64b91adc25c3a7409ffe0116f47
gooseinatophat <134507730+gooseinatophat@users.noreply.github.com>
2023-07-12 09:45:25 +0200

    it lives!

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-var second = 0;
-var minute = 0;
-var hour = 0;
-var d = new Date();
-var rotation = 45; 
+function openForm() {document.getElementById("myForm").style.display = "block";
+}
+/* function openForm() {document.getElementById("myForm").style.display = "block";
+} */
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+let s= 0;
+let min= 0;
+let h=0;
+let d= new Date();
+let um= 27.5;let uh = um*4; let ut=um/15;
 
 setInterval(
-    function(){
-        d = new Date
-        second = d.getSeconds() * 3;
-        minute = d.getMinutes() * 3;
-        hour = d.getHours() * 15 + Math.round(minute /24);
-        document.getElementById('hour-hand').style.transform ='rotate(' + hour + 'deg)';
-    },1000
-
+  function time () {
+    d=new Date();
+    s=d.getSeconds();
+    min=d.getMinutes();
+    h=d.getHours();
+    um=27.5;
+    uh=um*4;
+    ut=um/15;
+    document.getElementById("morning").style.width= min/15*um + uh*(h-6) + "px";
+    document.getElementById("noon").style.height=  min/15*um + uh*(h-12)  + "px";    
+    document.getElementById("eve").style.width=  min/15*um + uh*(h-18)  + "px";
+    document.getElementById("night").style.height=  min/15*um + uh*(h-24) + "px";
+    document.getElementById("seconds").innerHTML= d.getSeconds();
+  },100
 );
+
+if (new Date().getHours() > 12){document.getElementById("morning").style.width="660px";};
+if (new Date().getHours() > 18){document.getElementById("noon").style.height="660px";};
+if (new Date().getHours() > 24){document.getElementById("eve").style.width="660px";};
