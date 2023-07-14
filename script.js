62dc72150f567c73d8a29d53216b2afb306c5548
gooseinatophat <134507730+gooseinatophat@users.noreply.github.com>
2023-07-14 12:08:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-function openForm() {document.getElementById("myForm").style.display = "block";
-}
-function openIndex() {document.getElementById("iForm").style.display = "block";
-}
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
-function closeiForm() {
-    document.getElementById("iForm").style.display = "none";
-  }
+function openForm() {document.getElementById("myForm").style.display = "block";}
+function closeForm() {document.getElementById("myForm").style.display = "none";}
+
+function openInfo() {document.getElementById("iForm").style.display = "block";}
+function closeInfo() {document.getElementById("iForm").style.display = "none";}
+
+function openDescript(){document.getElementById("slot").style.display="block";}
+function closeIt(){document.getElementById("slot").style.display="none";}
+
+function openWork(){document.getElementById("workSlot").style.display="block";}
+function closeWork(){document.getElementById("workSlot").style.display="none";}
+
+function openFuel(){document.getElementById("fuelSlot").style.display="block";}
+function closeFuel(){document.getElementById("fuelSlot").style.display="none";}
+
+function openBreak(){document.getElementById("breakSlot").style.display="block";}
+function closeBreak(){document.getElementById("breakSlot").style.display="none";}
+
+function openCommute(){document.getElementById("commuteSlot").style.display="block";}
+function closeCommute(){document.getElementById("commuteSlot").style.display="none";}
+
+function openNap(){document.getElementById("napSlot").style.display="block";}
+function closeNap(){document.getElementById("napSlot").style.display="none";}
+
+function openSleep(){document.getElementById("sleepSlot").style.display="block";}
+function closeSleep(){document.getElementById("sleepSlot").style.display="none";}
+
+function openOther(){document.getElementById("otherSlot").style.display="block";}
+function closeOther(){document.getElementById("otherSlot").style.display="none";}
 
 let s= 0;
 let min= 0;
@@ -30,9 +49,8 @@ setInterval(
     document.getElementById("night").style.height=  min/15*um + uh*(h-24) + "px";
     document.getElementById("seconds").innerHTML= d.getSeconds();
     document.getElementById("time").innerHTML= h + ":" + min;
+    if (h > 12){document.getElementById("morning").style.width="660px";};
+    if (h > 18){document.getElementById("noon").style.height="660px";};
+    if (h > 24){document.getElementById("eve").style.width="660px";};
   },100
 );
-
-if (new Date().getHours() > 12){document.getElementById("morning").style.width="660px";};
-if (new Date().getHours() > 18){document.getElementById("noon").style.height="660px";};
-if (new Date().getHours() > 24){document.getElementById("eve").style.width="660px";};
