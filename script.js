10420bd69721868a4ca2c17d4fb2d53db18bf7ea
gooseinatophat <134507730+gooseinatophat@users.noreply.github.com>
2023-07-24 14:02:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -28,30 +28,181 @@ function closeSleep(){document.getElementById("sleepSlot").style.display="none";
 function openOther(){document.getElementById("otherSlot").style.display="block";}
 function closeOther(){document.getElementById("otherSlot").style.display="none";}
 
-function turnOn(){
-document.getElementById("workOnAm").style.display="block";
-document.getElementById("workOnPm").style.display="block";
-document.getElementById("lunch").style.display="block";
-document.getElementById("breakfast").style.display="block";
-document.getElementById("commute").style.display="block";
-document.getElementById("commuteFrom").style.display="block";
-document.getElementById("dinner").style.display="block";
-document.getElementById("nap").style.display="block";
-document.getElementById("breakAm").style.display="block";
-document.getElementById("breakPm").style.display="block";
+function toggleMenu(){
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
+  menu.classList.toggle('open');
+  menuToggle.classList.toggle('open');
+
+  if (menu.classList.contains('open'))
+  {menuToggle.textContent = '⌃';} 
+  else {menuToggle.textContent = '▼';}
 }
 
-function turnOff(){
-document.getElementById("workOnAm").style.display="none";
-document.getElementById("workOnPm").style.display="none";
-document.getElementById("lunch").style.display="none";
-document.getElementById("breakfast").style.display="none";
-document.getElementById("commute").style.display="none";
-document.getElementById("commuteFrom").style.display="none";
-document.getElementById("dinner").style.display="none";
-document.getElementById("nap").style.display="none";
-document.getElementById("breakAm").style.display="none";
-document.getElementById("breakPm").style.display="none";
+function morning(){
+  const bSlide = document.querySelector('.morningbtn');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  { document.getElementById("breakfast").style.display="block";
+    document.getElementById("exercise").style.display="block";
+    document.getElementById("commute").style.display="block";
+    document.getElementById("workOnAm").style.display="block";
+    document.getElementById("breakAm").style.display="block";} 
+  else {
+    document.getElementById("breakfast").style.display="none";
+    document.getElementById("commute").style.display="none";
+    document.getElementById("workOnAm").style.display="none";
+    document.getElementById("breakAm").style.display="none";
+    document.getElementById("exercise").style.display="none";}
+}
+
+function fourHwd(){
+  const bSlide = document.querySelector('.btn4hwd');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  {document.getElementById("lunch").style.display="block";
+  document.getElementById("workOnPm").style.display="block";  
+  document.getElementById("commuteFour").style.display="block";
+  document.getElementById("sensoryRestFour").style.display="block";
+  document.getElementById("journalA").style.display="block";
+  document.getElementById("meditateA").style.display="block";
+} 
+  else {document.getElementById("lunch").style.display="none";
+  document.getElementById("workOnPm").style.display="none";  
+  document.getElementById("commuteFour").style.display="none";
+  document.getElementById("sensoryRestFour").style.display="none";
+  document.getElementById("journalA").style.display="none";
+  document.getElementById("meditateA").style.display="none";}
+}
+function fiveHwd(){
+  const bSlide = document.querySelector('.btn5hwd');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  { document.getElementById("lunch").style.display="block";
+    document.getElementById("workOnPmFive").style.display="block";
+    document.getElementById("breakPm").style.display="block";
+    document.getElementById("commuteFive").style.display="block";
+    document.getElementById("sensoryRestFive").style.display="block";
+    document.getElementById("journalB").style.display="block";
+    document.getElementById("meditateB").style.display="block";} 
+  else {
+    document.getElementById("lunch").style.display="none";
+    document.getElementById("workOnPmFive").style.display="none";
+    document.getElementById("breakPm").style.display="none";
+    document.getElementById("commuteFive").style.display="none";
+    document.getElementById("sensoryRestFive").style.display="none";
+    document.getElementById("journalB").style.display="none";
+    document.getElementById("meditateB").style.display="none";}
+}
+function sixHwd(){
+  const bSlide = document.querySelector('.btn6hwd');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  {document.getElementById("lunch").style.display="block";
+  document.getElementById("workOnPmSix").style.display="block";  
+  document.getElementById("breakPm").style.display="block";
+  document.getElementById("commuteSix").style.display="block";
+  document.getElementById("sensoryRestSix").style.display="block";
+  document.getElementById("journalC").style.display="block";
+  document.getElementById("meditateC").style.display="block";} 
+  else {
+  document.getElementById("lunch").style.display="none";
+  document.getElementById("workOnPmSix").style.display="none";  
+  document.getElementById("breakPm").style.display="none";
+  document.getElementById("commuteSix").style.display="none";
+  document.getElementById("sensoryRestSix").style.display="none";
+  document.getElementById("journalC").style.display="none";
+  document.getElementById("meditateC").style.display="none";}
+}
+
+function evening(){
+  const bSlide = document.querySelector('.eveningbtn');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  { document.getElementById("snack").style.display="block";  
+  document.getElementById("nap").style.display="block";
+  document.getElementById("lateDinner").style.display="block";} 
+  else { document.getElementById("snack").style.display="none";  
+  document.getElementById("nap").style.display="none";
+  document.getElementById("lateDinner").style.display="none";}
+}
+
+function errandsA(){
+  const bSlide = document.querySelector('.errandbtn4');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  {document.getElementById("errands4").style.display="block";
+  document.getElementById("commuteE4").style.display="block";
+  document.getElementById("sensoryRestFour").style.display="none";}
+  else {document.getElementById("errands4").style.display="none";
+  document.getElementById("commuteE4").style.display="none";}
+}
+function errandsB(){
+  const bSlide = document.querySelector('.errandbtn5');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  {document.getElementById("errands5").style.display="block";
+  document.getElementById("commuteE5").style.display="block";
+  document.getElementById("sensoryRestFive").style.display="none";}
+  else {document.getElementById("errands5").style.display="none";
+  document.getElementById("commuteE5").style.display="none";}
+}
+function errandsC(){
+  const bSlide = document.querySelector('.errandbtn6');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  {document.getElementById("errands6").style.display="block";
+  document.getElementById("commuteE6").style.display="block";
+  document.getElementById("sensoryRestSix").style.display="none";}
+  else {document.getElementById("errands6").style.display="none";
+  document.getElementById("commuteE6").style.display="none";}
+}
+function waterATwo(){
+  const bSlide = document.querySelector('.wa2');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  { document.getElementById("lakeOrRiver4").style.display="block";
+  document.getElementById("commuteToW").style.display="block";
+  document.getElementById("commuteW").style.display="block";} 
+  else { document.getElementById("lakeOrRiver4").style.display="none";
+  document.getElementById("commuteToW").style.display="none";
+  document.getElementById("commuteW").style.display="none";}
+}
+function waterAThree(){
+  const bSlide = document.querySelector('.wa3');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  { document.getElementById("lakeOrRiver5").style.display="block";
+  document.getElementById("commuteW").style.display="block";} 
+  else { document.getElementById("lakeOrRiver5").style.display="none";
+  document.getElementById("commuteW").style.display="none";}
+}
+function waterAFour(){
+  const bSlide = document.querySelector('.wa4');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  {document.getElementById("lakeOrRiver6").style.display="block";}
+  else { document.getElementById("lakeOrRiver6").style.display="none";}
+}
+
+function something(){
+  const bSlide = document.querySelector('.smth');
+  bSlide.classList.toggle('open');
+
+  if (bSlide.classList.contains('open'))
+  {} 
+  else {}
 }
 
 
@@ -76,8 +227,8 @@ setInterval(
     document.getElementById("night").style.height=  min/15*um + uh*(h-24) + "px";
     document.getElementById("seconds").innerHTML= d.getSeconds();
     document.getElementById("time").innerHTML= h + ":" + min;
-    if (h > 12){document.getElementById("morning").style.width="660px";};
-    if (h > 18){document.getElementById("noon").style.height="660px";};
-    if (h > 24){document.getElementById("eve").style.width="660px";};
+    if (h > 11){document.getElementById("morning").style.width="660px";};
+    if (h > 17){document.getElementById("noon").style.height="660px";};
+    if (h > 23){document.getElementById("eve").style.width="660px";};
   },100
 );
